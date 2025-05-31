67c218542b7beb519b9204641116e920ace4cc69
SanqDuonq <namsang0902s@gmail.com>
2025-05-31 22:15:51 +0700

    feat: constant

---
+++ lib/core/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ConstantText {
+  static TextStyle titleText = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.5,
+    height: 1,
+    color: Color(0xFF1A1A1A)
+  );
+  static TextStyle descriptionText = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0,
+    height: 1.4,
+    color: Color.fromRGBO(0, 0, 0, 0.6)
+  );
+}
