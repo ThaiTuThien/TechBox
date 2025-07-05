6b120c9405224dd123ca30eea3afdb6b3ed5a9ad
ThaiTuThien <thuanthien2404@gmail.com>
2025-07-05 13:25:59 +0700

    feat: add cart and empty cart screens with custom app bar

---
+++ lib/screens/cart_empty.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:techbox/components/app_bar.dart';
+
+class CartEmpty extends StatelessWidget {
+  const CartEmpty({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBarCart(),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/image/cart.png',
+                width: 64,
+                height: 64,
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Giỏ hàng trống',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Hãy thêm sản phẩm vào giỏ hàng',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color.fromARGB(128, 128, 128, 128),
+                ),
+              ),
+            ],
+          ),
+        ),
+    );
+  }
+}
